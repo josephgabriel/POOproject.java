f211f63f3339390c9dbabce055838af4926f0b99
João Arthur <153871082+ArthurX10@users.noreply.github.com>
2025-07-13 23:48:55 -0300

    shiiii
    Criação das classes venda e item venda

---
+++ Venda.java
@@ -0,0 +1,99 @@
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Venda{
+
+    private long id;
+    private Cliente cliente;
+    private List<ItemVenda> itens = new ArrayList<>();// vou criar dps uma lista de item, pq agora só está calculando 1 produto só
+    private double quantidadeTotal;
+    private BigDecimal valor;
+    private BigDecimal valorPago;
+    private BigDecimal troco;
+    private Date dataVenda = new Date();
+    private boolean aprovada;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public List<ItemVenda> getItens() {
+        return itens;
+    }
+
+    public void setItens(List<ItemVenda> itens) {
+        this.itens = itens;
+    }
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public void setValor(BigDecimal valor) {
+        this.valor = valor;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public double getQuantidadeTotal() {
+        return quantidadeTotal;
+    }
+
+    public void setQuantidadeTotal(double quantidadeTotal) {
+        this.quantidadeTotal = quantidadeTotal;
+    }
+
+    public BigDecimal getvalor() {
+        return valor;
+    }
+
+    public void setvalor(BigDecimal valor) {
+        this.valor = valor;
+    }
+
+    public BigDecimal getValorPago() {
+        return valorPago;
+    }
+
+    public void setValorPago(BigDecimal valorPago) {
+        this.valorPago = valorPago;
+    }
+
+    public BigDecimal getTroco() {
+        return troco;
+    }
+
+    public void setTroco(BigDecimal troco) {
+        this.troco = troco;
+    }
+
+    public Date getDataVenda() {
+        return dataVenda;
+    }
+
+    public void setDataVenda(Date dataVenda) {
+        this.dataVenda = dataVenda;
+    }
+
+    public boolean isAprovada() {
+        return aprovada;
+    }
+
+    public void setAprovada(boolean aprovada) {
+        this.aprovada = aprovada;
+    }
+
+  // ainda falta implementar os metodos
+}
