836a4ab28f61ba2ddb2cc544bb2f456711d8d24a
José Gabriel Soares dos Santos <josgabrielsoares825@gmail.com>
2025-08-01 20:38:30 -0300

    Create Venda.java

---
+++ Back-end/Classes/Venda.java
@@ -0,0 +1,45 @@
+package classes;
+
+public class ItemVenda {
+    private int id;
+    private Produto produto;
+    private Venda venda;
+    private int quantidade;
+    private double precoUnitario;
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+
+    public Venda getVenda() {
+        return venda;
+    }
+
+    public void setVenda(Venda venda) {
+        this.venda = venda;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public double getPrecoUnitario() {
+        return precoUnitario;
+    }
+
+    public void setPrecoUnitario(double precoUnitario) {
+        this.precoUnitario = precoUnitario;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
